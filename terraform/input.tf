af01d96fcca016aece562c760d3b78e4a1c2a553
Konrad Semsch <konrad.semsch@innogy.com>
2021-11-21 20:31:19 +0100

    debug

--- terraform/input.tf
+++ terraform/input.tf
@@ -5,12 +5,30 @@ variable "client_secret" {}
 variable "tenant_id" {}
 
 # Resource Group/Location
-variable "location" {}
-variable "resource_group" {}
-variable "application_type" {}
+variable "location" {
+    type        = string
+    default     = "West Europe"
+}
+variable "resource_group" {
+    type        = string
+    default     = "quality-release-rg"
+}
+variable "application_type" {
+    type        = string
+    default     = "quality-release-app"
+}
 
 # Network
-variable virtual_network_name {}
-variable address_prefix_test {}
-variable address_space {}
+variable virtual_network_name {
+    type        = string
+    default     = "quality-relase-vm"
+}
+variable address_prefix_test {
+    type        = string
+    default     = "10.5.1.0/24"
+}
+variable address_space {
+    type        = list(string)
+    default     = ["10.5.0.0/16"]
+}
 
