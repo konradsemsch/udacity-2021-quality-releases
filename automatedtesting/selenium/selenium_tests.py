37f088cd1739e9452362513c90692cdd1714e044
Konrad Semsch <konrad.semsch@innogy.com>
2021-12-09 08:29:36 +0100

    expanded the selenium test suite

--- automatedtesting/selenium/selenium_tests.py
+++ automatedtesting/selenium/selenium_tests.py
@@ -1,16 +1,45 @@
 # #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.by import By
 
 
 def login(driver, user, password):
     """Login into the website with given user an password"""
     driver.get("https://www.saucedemo.com/")
-    driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
-    driver.find_element_by_css_selector("input[id='password']").send_keys(password)
-    driver.find_element_by_id("login-button").click()
+    driver.find_element_by(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
+    driver.find_element_by(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
+    driver.find_element_by(By.ID, "login-button").click()
     assert "https://www.saucedemo.com/inventory.html" in driver.current_url
     print(f"Successfully logged into the website with user: {user} and password: {password}")
+
+def add_to_cart(driver, n):
+    """Add items to cart sequentially using the item number."""
+    counter = 0
+    for i in range(n):
+        element = "a[id='item_" + str(i) + "_title_link']"
+        driver.find_element_by(By.CSS_SELECTOR, element).click()
+        driver.find_element_by(By.CSS_SELECTOR, "button.btn_primary.btn_inventory").click()
+        product = driver.find_element_by(By.CSS_SELECTOR, "div[class='inventory_details_name large_size']").text
+        print(f"Added {product} to cart")
+        driver.find_element_by(By.CSS_SELECTOR, "button.inventory_details_back_button").click()
+        counter += 1
+    assert counter == n
+    return counter
+
+def remove_from_cart(driver, n):
+    """Remove items from cart sequentially using the item number."""
+    counter = 0
+    for i in range(n):
+        element = "a[id='item_" + str(i) + "_title_link']"
+        driver.find_element_by(By.CSS_SELECTOR, element).click()
+        driver.find_element_by(By.CSS_SELECTOR, "button.btn_secondary.btn_inventory").click()
+        product = driver.find_element_by(By.CSS_SELECTOR, "div[class='inventory_details_name large_size']").text
+        print(f"Removed {product} from cart")
+        driver.find_element_by(By.CSS_SELECTOR, "button.inventory_details_back_button").click()
+        counter += 1
+    assert counter == n
+    return counter
 
 if __name__ == "__main__":
     print("Starting the browser...")
@@ -21,4 +50,7 @@ if __name__ == "__main__":
 
     # Starting test-suite functions
     login(driver=driver, user="standard_user", password="secret_sauce")
+    n_items_added = add_to_cart(driver, 6)
+    n_items_removed = remove_from_cart(driver, n_items_added)
+    assert n_items_added == n_items_removed
 
