0f8040500b27ca37c0703bb39fe2d424a0bf41a5
Konrad Semsch <konrad.semsch@innogy.com>
2021-12-11 14:28:46 +0100

    fixed tests syntax

--- automatedtesting/selenium/selenium_tests.py
+++ automatedtesting/selenium/selenium_tests.py
@@ -1,4 +1,5 @@
 # #!/usr/bin/env python
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
@@ -7,9 +8,9 @@ from selenium.webdriver.common.by import By
 def login(driver, user, password):
     """Login into the website with given user an password"""
     driver.get("https://www.saucedemo.com/")
-    driver.find_element_by(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
-    driver.find_element_by(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
-    driver.find_element_by(By.ID, "login-button").click()
+    driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
+    driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
+    driver.find_element(By.ID, "login-button").click()
     assert "https://www.saucedemo.com/inventory.html" in driver.current_url
     print(f"Successfully logged into the website with user: {user} and password: {password}")
 
@@ -18,11 +19,11 @@ def add_to_cart(driver, n):
     counter = 0
     for i in range(n):
         element = "a[id='item_" + str(i) + "_title_link']"
-        driver.find_element_by(By.CSS_SELECTOR, element).click()
-        driver.find_element_by(By.CSS_SELECTOR, "button.btn_primary.btn_inventory").click()
-        product = driver.find_element_by(By.CSS_SELECTOR, "div[class='inventory_details_name large_size']").text
+        driver.find_element(By.CSS_SELECTOR, element).click()
+        driver.find_element(By.CSS_SELECTOR, "button.btn_primary.btn_inventory").click()
+        product = driver.find_element(By.CSS_SELECTOR, "div[class='inventory_details_name large_size']").text
         print(f"Added {product} to cart")
-        driver.find_element_by(By.CSS_SELECTOR, "button.inventory_details_back_button").click()
+        driver.find_element(By.CSS_SELECTOR, "button.inventory_details_back_button").click()
         counter += 1
     assert counter == n
     return counter
@@ -32,11 +33,11 @@ def remove_from_cart(driver, n):
     counter = 0
     for i in range(n):
         element = "a[id='item_" + str(i) + "_title_link']"
-        driver.find_element_by(By.CSS_SELECTOR, element).click()
-        driver.find_element_by(By.CSS_SELECTOR, "button.btn_secondary.btn_inventory").click()
-        product = driver.find_element_by(By.CSS_SELECTOR, "div[class='inventory_details_name large_size']").text
+        driver.find_element(By.CSS_SELECTOR, element).click()
+        driver.find_element(By.CSS_SELECTOR, "button.btn_secondary.btn_inventory").click()
+        product = driver.find_element(By.CSS_SELECTOR, "div[class='inventory_details_name large_size']").text
         print(f"Removed {product} from cart")
-        driver.find_element_by(By.CSS_SELECTOR, "button.inventory_details_back_button").click()
+        driver.find_element(By.CSS_SELECTOR, "button.inventory_details_back_button").click()
         counter += 1
     assert counter == n
     return counter
